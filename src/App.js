461325238e09f08c6189b8e10bb3da2596a8e855
OlgaSavytska <o.savytska77@gmail.com>
2019-10-16 09:26:20 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import './App.css';
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo1 from '../../images/Rectangle-1.png';
+import logo2 from '../../images/Rectangle.png';
+import menu from '../../images/Menu.png';
+import s from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={s.header_cont}>
+      <div className={s.logo_cont}>
+        <img className={s.logo1} src={logo1} alt="logo1" />
+        <img className={s.logo2} src={logo2} alt="logo2" />
+      </div>
+      <div className={s.menu_cont}>
+        <img className={s.menu} src={menu} alt="menu" />
+        <div className={s.button_cont}>
+          <button className={s.button}>Замовити картку</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
