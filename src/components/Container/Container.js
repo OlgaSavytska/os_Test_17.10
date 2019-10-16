f94562e89d2834dd98015dca6cd1497e59a85a67
OlgaSavytska <o.savytska77@gmail.com>
2019-10-16 17:49:29 +0300

    3

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -6,7 +6,20 @@ import s from './Container.module.css';
 const Container = () => {
   return (
     <div className={s.cont}>
-      <ReactPlayer url={d} playing className={s.j} width="100%" height="100%" />
+      <ReactPlayer
+        url={d}
+        autoPlay
+        playing
+        className={s.j}
+        width="100%"
+        height="100%"
+      />
+      <div className={s.for_background}>
+        <div className={s.title_cont}>
+          <p className={s.title}>Visa Signature</p>
+          <h2 className={s.title_big}>ТВОЯ КАРТКА ДЛЯ ПОДОРОЖЕЙ</h2>
+        </div>
+      </div>
     </div>
   );
 };
