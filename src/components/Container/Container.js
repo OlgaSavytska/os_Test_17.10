a3a4d3098e39efa9dfe08919d4a4eae359a50042
OlgaSavytska <o.savytska77@gmail.com>
2019-10-17 10:29:25 +0300

    6

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,8 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import ReactPlayer from 'react-player';
 import d from '../../images/video.mp4';
 import s from './Container.module.css';
 import card from '../../images/Group.png';
+import mini from '../../images/MaskGroup.png';
 
 const Container = () => {
   return (
@@ -33,7 +36,26 @@ const Container = () => {
                 Ретельна підготовка - запорука вдалої мандрівки.
               </p>
               <div className={s.table_content}>
-                <div className={s.table_content_contr} />
+                <div className={s.table_content_contr}>
+                  <ul className={s.items}>
+                    <li className={s.item}>
+                      <a href="" className={s.link}>
+                        <img className={s.img1} src={mini} alt="mini card" />
+                        <h2 className={s.title_item}>
+                          Visa Luxury Hotel collection
+                        </h2>
+                        <p className={s.item_descr}>
+                          Діє по всьому світу цілодобово й надає для вас
+                          глобальний сервіс інформаційної підтримки в
+                          телефонному режимі.
+                        </p>
+                        <a href="" className={s.link1}>
+                          Подробнее >
+                        </a>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
